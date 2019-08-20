ebf7032a18040a8ee909c9b3bb71a14d3c2bf144
Bo Sun <sunbo804400200@gmail.com>
2019-08-20 15:16:05 -0400

    Trying to use ML to recog text

--- money.py
+++ money.py
@@ -1,10 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import TrainAndTest
 
 cap = cv2.VideoCapture('./test.mp4')
-shot = np.zeros((25,80))
-frame_cntr = 0
+shot = np.zeros((50, 160))
+frame_cntr = 0#frame counter to exract 
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))#erosion kernal
 while(cap.isOpened()):
     ret, img = cap.read()
     if (ret == False):
@@ -12,16 +14,21 @@ while(cap.isOpened()):
     else:
         frame_cntr = frame_cntr+1
         money_region = img[1045:1070, 1200:1280]
+        money_region = cv2.resize(money_region, (160, 50))
         money_region_gray = cv2.cvtColor(money_region, cv2.COLOR_BGR2GRAY)
         _, money_region_binary = cv2.threshold(money_region_gray, 
-                                                200, #threshold = 200
+                                                100, #threshold = 200
                                                 255, #max value in image
                                                 cv2.THRESH_BINARY)#binary thresh
         #######got the money########
         money = money_region_binary
-        cv2.imshow('money',money_region_binary)
-        if (frame_cntr == 25):
-            shot = np.concatenate((shot, money_region_binary), axis=0)
+        erosion = cv2.erode(money,kernel)
+        closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
+        cv2.imshow('money',erosion)
+        number = TrainAndTest
+        if (frame_cntr == 30):
+            shot = np.concatenate((shot, closing), axis=0)
+            print(number)
             frame_cntr = 0
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- skill.py
+++ skill.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('./test_pictures/test4.png')
+img = cv2.imread('./test_pictures/test2.png')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #bottom_region = bars_region(img)
